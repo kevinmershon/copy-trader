e1ff32e5ed488c071fb5329ee81d47fbe59b2a50
Kevin Mershon <kevin@mershonenterprises.com>
2022-04-22 14:10:29 -0700

    Continue building out alpaca driver. Broken build.

--- src/clj/copy_trader/exchange/alpaca/trader.clj
+++ src/clj/copy_trader/exchange/alpaca/trader.clj
@@ -30,6 +30,7 @@
 
 (defmethod on-trade ["alpaca" "long" "limit"]
   [{nickname :nickname :as trader-map} {:keys [symbol price stop-loss]}]
+  ;; FIXME -- cancel existing long order if it exists
   (let [volume (compute-volume trader-map)
         order-data (driver/alpaca-post!
                     trader-map "orders"
@@ -46,20 +47,44 @@
         (log/error (format "%s failed to limit long for %s" nickname symbol))
         (log/error (:body order-data)))
       (let [tx-id (get-in order-data [:body :id])]
-        (log/info (format "%s: spot buying %.2f %s at price %.2f (stop-loss at %.2f)"
+        (log/info (format "%s: longing %.2f %s at price %.2f (stop-loss at %.2f)"
                           nickname volume symbol price stop-loss))
         ;; FIXME -- add order to order log
         (comment "record the order")))))
 
 (defmethod on-trade ["alpaca" "long" "take-profit"]
-  [trader-map {:keys [symbol price stop-loss percentage]}]
-  (comment
-    "cancel the current stop-loss, take profit, and open a new stop-loss"))
+  [{nickname :nickname :as trader-map} {:keys [symbol percentage] :as ev}]
+  ;; FIXME -- cancel-existing long stop-loss
+  (let [volume 0] ;; FIXME -- get existing volume and multiply by percentage
+    (driver/alpaca-post!
+     trader-map "orders"
+     {:symbol        symbol
+      :side          "sell"
+      :type          "market"
+      :time_in_force "gtc"
+      :qty           (int volume)})
+    (on-trade trader-map (assoc ev :order-type "stop-loss"))))
 
 (defmethod on-trade ["alpaca" "long" "stop-loss"]
-  [trader-map {:keys [symbol stop-loss]}]
-  (comment
-    "cancel the current stop-loss, and place a new one"))
+  [{nickname :nickname :as trader-map} {:keys [symbol stop-loss]}]
+  ;; FIXME -- cancel-existing long stop-loss
+  (let [volume 0 ;; get existing volume
+        order-data (driver/alpaca-post!
+                    trader-map "orders"
+                    {:symbol        symbol
+                     :side          "sell"
+                     :type          "stop"
+                     :time_in_force "gtc"
+                     :stop_price    stop-loss
+                     :qty           (int volume)})]
+    (if (<= 400 (:status order-data))
+      (do
+        (log/error (format "%s: failed to open stop-loss order for %s" nickname symbol))
+        (log/error (:body order-data)))
+      (let [tx-id (get-in order-data [:body :id])]
+        ;; FIXME -- add order to order log
+        (log/info (format "%s: set %s long stop-loss to %.2f"
+                          nickname symbol stop-loss))))))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;
 ;;                                   shorts                                   ;;
--- src/clj/copy_trader/exchange/trader.clj
+++ src/clj/copy_trader/exchange/trader.clj
@@ -6,7 +6,7 @@
     exchange))
 
 (defmulti on-trade
-  (fn [{:keys [exchange _symbol direction order-type _price _stop-loss]}]
+  (fn [{:keys [exchange _symbol direction order-type _price _stop-loss _percentage]}]
     [exchange direction order-type]))
 
 (defmethod on-trade :default
