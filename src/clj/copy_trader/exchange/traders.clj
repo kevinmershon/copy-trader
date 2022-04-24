cc183be45597acfd34149677b69c54a861aa1fd6
Kevin Mershon <kevin@mershonenterprises.com>
2022-04-24 13:20:52 -0700

    Don't take shorts if the trader is configured not to

--- src/clj/copy_trader/exchange/traders.clj
+++ src/clj/copy_trader/exchange/traders.clj
@@ -23,9 +23,11 @@
   (:traders @core/state))
 
 (defn dispatch-trade!
-  [{:keys [asset-type _symbol _direction _order-type _price _stop-loss _percentage] :as trade-msg}]
+  [{:keys [asset-type _symbol direction _order-type _price _stop-loss _percentage] :as trade-msg}]
   (let [traders (->> (:traders @core/state)
                      (filter #(some #{asset-type} (:assets (deref %)))))]
     (doseq [trader traders]
-      (let [{:keys [exchange] :as trader-map} @trader]
-        (trader/on-trade trader-map (assoc trade-msg :exchange exchange))))))
+      (let [{:keys [exchange shorting] :as trader-map} @trader]
+        (when (or (= "long" direction)
+                  (and shorting (= "short" direction)))
+          (trader/on-trade trader-map (assoc trade-msg :exchange exchange)))))))
