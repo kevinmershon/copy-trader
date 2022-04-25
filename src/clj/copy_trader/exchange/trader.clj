7d15400ed142b3218a0833c6391322c78e1e6156
Kevin Mershon <kevin@mershonenterprises.com>
2022-04-25 08:50:00 -0700

    Fix calculation of volume for positions

--- src/clj/copy_trader/exchange/trader.clj
+++ src/clj/copy_trader/exchange/trader.clj
@@ -51,21 +51,23 @@
       0.0)))
 
 (defn compute-volume
-  [{:keys [balance-usd risk] :as _trader-map} price high low]
-  (let [diff                 (- high low)
+  [{:keys [risk leverage max_positions] :as trader-map} price high low]
+  (let [diff               (- high low)
+        balance-usd        (/ (* (:balance-usd trader-map) leverage)
+                              max_positions)
         ;; use a minimum of the capital allocated, or don't take the trade
-        min-usage            (/ (* balance-usd 0.20)
-                                price)
-        max-usage            balance-usd
+        min-usage          (/ (* balance-usd 0.20)
+                              price)
+        max-usage          balance-usd
         ;; we never want to risk more than our tolerance
-        max-risk             (* risk max-usage)
-        max-trade-size       (if (pos? diff)
-                               (/ max-risk diff)
-                               0.0)
+        max-risk           (* risk max-usage)
+        max-trade-size     (if (pos? diff)
+                             (/ max-risk diff)
+                             0.0)
         ;; this is the maximum amount we COULD buy
-        max-purchase-power   (/ max-usage price)
+        max-purchase-power (/ max-usage price)
         ;; but this is the max we CHOOSE to buy
-        desired-volume       (min max-purchase-power max-trade-size)]
+        desired-volume     (min max-purchase-power max-trade-size)]
     ;; don't take the trade unless we're meeting minimum volume requirements
     (if (> desired-volume min-usage)
       desired-volume
